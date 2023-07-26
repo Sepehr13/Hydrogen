88fdd29163d958da00fa5d376bd942331c500498
Sepehr Azizi <sepehrazizi13@gmail.com>
2023-07-27 00:33:43 +0330

    added JsDoc to public functions

--- src/hydrogen/hydrogen.ts
+++ src/hydrogen/hydrogen.ts
@@ -22,7 +22,7 @@ class Hydrogen {
     /**
      * Defines the destination of the request.
      * @example ['user', 'getMe'] // converts to user/getMe
-     * @example ['user', ':firstName', ':lastName'] // use with DestinationParamMap will convert it to user/Max/Payne
+     * @example ['user', ':firstName', ':lastName'] // using with DestinationParamMap will convert it to user/Max/Payne
      * @param { string[] } destination - Destination of the request to be sent.
      * @returns { Hydrogen } Instance of Hydrogen
      */
@@ -45,31 +45,63 @@ class Hydrogen {
         return Hydrogen.instance;
     }
 
+    /**
+     * Change the server location which will recieve the request.
+     * @example https://localhost:3000/
+     * @example https://remote.server.com/
+     * @param { string } baseUrl - URL of the server.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     public BaseUrl(baseUrl: string): Hydrogen {
         this.options.baseUrl = baseUrl;
         return Hydrogen.instance;
     }
 
+    /**
+     * Set query params for the request.
+     * @example {page: '1', sort: 'ASC'}
+     * @param { object } queryParams - Query params object.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     public QueryParams(queryParams: object): Hydrogen {
         this.options.queryParams = queryParams;
         return Hydrogen.instance;
     }
 
+    /**
+     * The data to be sent along with the request.
+     * @param { string | object | ArrayBuffer | ArrayBufferView | URLSearchParams | FormData | File | Blob } data - Data to be sent.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     public Data(data: string | object | ArrayBuffer | ArrayBufferView | URLSearchParams | FormData | File | Blob): Hydrogen {
         this.options.data = data;
         return Hydrogen.instance;
     }
 
+    /**
+     * Request will be canceled after said time if it didn't bring any answer from server.
+     * @param { number } timeout - Timeout in milliseconds.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     public Timeout(timeout: number): Hydrogen {
         this.options.timeout = timeout;
         return Hydrogen.instance;
     }
 
+    /**
+     * Retry the request if it failed.
+     * @param { number } retry - How many times to retry.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     public Retry(retry: number): Hydrogen {
         this.options.retry = retry;
         return Hydrogen.instance;
     }
 
+    /**
+     * Build the request and get it ready to be sent.
+     * @returns { AxiosEngine } A copy of your request with HTTP engine built in it.
+     */
     public Build(): AxiosEngine {
         let optionsClone = this.options
         this.options = new HydrogenOptions();
@@ -79,11 +111,15 @@ class Hydrogen {
                 break;
         
             default:
-                throw new Error("Method Not Implemented!");
+                throw new Error("Engine Not Implemented!");
                 break;
         }
     }
 
+    /**
+     * Get a singleton instance of Hydrogen.
+     * @returns { Hydrogen } Instance of Hydrogen
+     */
     private static getInstance(): Hydrogen {
         if (!Hydrogen.instance) {
             Hydrogen.instance = new Hydrogen();
